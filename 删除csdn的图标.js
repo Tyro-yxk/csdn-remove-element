150d030bb735737dc0f1a3be07415216daf3e5c9
Tyro-yxk <tyroyang@foxmail.com>
2025-05-29 22:53:08 +0800

    1111

--- 删除csdn的图标.js
+++ 删除csdn的图标.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name         删除csdn的AI写代码和右下角deepseek和手机图标
-// @namespace    http://tampermonkey.net/
-// @version      2025-05-29
+// @namespace    https://github.com/Tyro-yxk/csdn-remove-element
+// @version      0.0.1
 // @description  try to take over the world!
 // @author       tyro-yxk
 // @match        http*://blog.csdn.net/*/article/details/*
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAb1BMVEX8VTH8USz8Tyj8Tif8Uy78TST8eF38kn79mIb8fGP8Wjb+zsb+493/8/D+6+j+0sv9nYr8b1L/9/X////9xbj9va7+3NP9qpn8Zkj+6OL8Rhb8Sx/9x7v8gmr8YD39pJT8iHD/+vn+tqn8j3j9sKPVly74AAAA5klEQVR4Aa2SRZLDQBAENSQcsKbErJX//8Ulk3DRec2uiCbnSRDKKOF7hgvH9XwvCNlOyvEjqZSW5rQJ0ziyuGCNWDsJwCZpBgD5wpKiBJRX1XVVREBSz2XdAGUrLh2nOibzYAzY07VLTkO6CEaA6e49LprloYLthbMLHUZol+xL0Y+QzgH1CyCrv0iRA5IcSDqtGpoX8s5iPNcPFbOZrRNATzW/nDU05UDnUQDqHFbvOCcNZPP2WHu5pJQKHzT1cklyxA0VdXzZcZEbqZXSZdZ49fpPiAjdyfe9tmB07/0IZZfX/DdvphAP/wvKn48AAAAASUVORK5CYII=
 // @grant        none
+// @updateURL
+// @downloadURL https://github.com/Tyro-yxk/csdn-remove-element/blob/master/%E5%88%A0%E9%99%A4csdn%E7%9A%84%E5%9B%BE%E6%A0%87.js
+// @supportURL  https://github.com/Tyro-yxk/csdn-remove-element/issues
 // ==/UserScript==
 
 (function() {
