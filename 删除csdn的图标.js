b372dc6b97d0562db8edaff0f50db8bd2334363e
Tyro-yxk <tyroyang@foxmail.com>
2025-06-12 01:20:22 +0800

    111111

--- 删除csdn的图标.js
+++ 删除csdn的图标.js
@@ -7,17 +7,24 @@
 // @match        http*://blog.csdn.net/*/article/details/*
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAb1BMVEX8VTH8USz8Tyj8Tif8Uy78TST8eF38kn79mIb8fGP8Wjb+zsb+493/8/D+6+j+0sv9nYr8b1L/9/X////9xbj9va7+3NP9qpn8Zkj+6OL8Rhb8Sx/9x7v8gmr8YD39pJT8iHD/+vn+tqn8j3j9sKPVly74AAAA5klEQVR4Aa2SRZLDQBAENSQcsKbErJX//8Ulk3DRec2uiCbnSRDKKOF7hgvH9XwvCNlOyvEjqZSW5rQJ0ziyuGCNWDsJwCZpBgD5wpKiBJRX1XVVREBSz2XdAGUrLh2nOibzYAzY07VLTkO6CEaA6e49LprloYLthbMLHUZol+xL0Y+QzgH1CyCrv0iRA5IcSDqtGpoX8s5iPNcPFbOZrRNATzW/nDU05UDnUQDqHFbvOCcNZPP2WHu5pJQKHzT1cklyxA0VdXzZcZEbqZXSZdZ49fpPiAjdyfe9tmB07/0IZZfX/DdvphAP/wvKn48AAAAASUVORK5CYII=
 // @grant        GM_log
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_addStyle
+// @grant       GM_deleteValue
+// @grant       GM_xmlhttpRequest
+// @grant       GM_setClipboard
+// @grant       GM_registerMenuCommand
 // @updateURL
 // @downloadURL https://github.com/Tyro-yxk/csdn-remove-element/blob/master/%E5%88%A0%E9%99%A4csdn%E7%9A%84%E5%9B%BE%E6%A0%87.js
 // @supportURL  https://github.com/Tyro-yxk/csdn-remove-element/issues
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    // 直接删除ai写代码后的复制按钮很丑
-    function overrideTargetStyle() {
-        const style = document.createElement('style');
-        style.innerHTML = `
+(function () {
+  "use strict";
+  // 直接删除ai写代码后的复制按钮很丑
+  function overrideTargetStyle() {
+    const style = document.createElement("style");
+    style.innerHTML = `
             main div.blog-content-box pre .hljs-button.active {
                 width: initial !important;
             }
@@ -29,38 +36,46 @@
                 word-break: break-all;
                 margin-left: auto !important;
             }
-                .hljs-button{
+            .hljs-button{
                 margin-left: auto !important;
-                }
-        `;
-        document.head.appendChild(style);
-        console.log('已添加覆盖样式');
-    }
-
-    function safeRemoveElement(selector, retryDelay = 100, maxRetries = 10) {
-        let retries = 0;
-        function attemptRemove() {
-            const elements = document.querySelectorAll(selector);
-            if (elements) {
-                elements.forEach(element => {
-                    element.remove();
-                });
-                console.log(`成功移除 ${selector}`);
-            } else if (retries < maxRetries) {
-                retries++;
-                console.warn(`未找到 ${selector}, 重试中... (${retries}/${maxRetries})`);
-                setTimeout(attemptRemove, retryDelay);
-            } else {
-                console.error(`无法找到 ${selector} 元素`);
             }
-        }
+           #csdn-toolbar .toolbar-inside.exp3 .toolbar-container-middle .toolbar-search .toolbar-search-container input{
+              width:calc(86% + 2px)
+           }
+        `;
+    document.head.appendChild(style);
+    console.log("已添加覆盖样式");
+  }
 
-        attemptRemove();
+  function safeRemoveElement(selector, retryDelay = 100, maxRetries = 10) {
+    let retries = 0;
+    function attemptRemove() {
+      const elements = document.querySelectorAll(selector);
+      if (elements) {
+        elements.forEach((element) => {
+          element.remove();
+        });
+        console.log(`成功移除 ${selector}`);
+      } else if (retries < maxRetries) {
+        retries++;
+        console.warn(
+          `未找到 ${selector}, 重试中... (${retries}/${maxRetries})`
+        );
+        setTimeout(attemptRemove, retryDelay);
+      } else {
+        console.error(`无法找到 ${selector} 元素`);
+      }
     }
-    safeRemoveElement('.btn-code-notes');
-    safeRemoveElement('.code-run-btn');
-    overrideTargetStyle()
-    
-    
 
+    attemptRemove();
+  }
+  setTimeout(() => {
+    safeRemoveElement(".btn-code-notes");
+    safeRemoveElement(".code-run-btn");
+    safeRemoveElement(".sidecolumn-vip");
+    safeRemoveElement(".option-box");
+    safeRemoveElement("#sidecolumn-deepseek");
+    safeRemoveElement("#toolbar-c-box-button");
+    overrideTargetStyle();
+  }, 1000);
 })();
